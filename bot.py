467a3bb176fd7091bcb1997552711e9741c60641
Hikari-First <68914254+Hikari-First@users.noreply.github.com>
2020-08-09 13:26:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,12 +11,31 @@ async def join(ctx):
     channel = ctx.message.author.voice.channel
     await channel.connect()
 
-@bot.command()
+@bot.command(pass_context = True)
 async def play(ctx):
-    channel = ctx.message.author.voice.channel
-    vc = await channel.connect()
-    vc.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: print('done', e))
-    vc.is_playing()
+
+    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+
+    if voice_client.is_connected:
+        song_there = os.path.isfile('song.mp3')
+
+        await ctx.send("Pls wait, it won't take long.")
+
+        voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+
+        voice.play(discord.FFmpegPCMAudio('song.mp3'), after = lambda e: print(f'[LOG]: {name}, music ended.'))
+
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+
+        voice.source.volume = 0.07
+
+        song_name = name.rsplit('-', 2)
+
+        await ctx.send(f'Np: {song_name[0]}')
+
+    else:
+
+        await ctx.send('I`m not connected to ur VC!')
 
 token = os.environ.get('BOT_TOKEN')
 bot.run(token)
